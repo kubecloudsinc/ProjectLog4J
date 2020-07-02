53c349ead02b19534a1712937e0990828e669d33
p9madhavi <p9.madhavi@gmail.com>
2020-07-02 17:02:34 -0400

    commit number two

--- src/main/java/com/apps/Log4jTest.java
+++ src/main/java/com/apps/Log4jTest.java
@@ -7,7 +7,9 @@ public class Log4jTest {
     private static final Logger LOG = Logger.getLogger(Log4jTest.class);
 
     public static void main(String[] args) {
+
         LOG.debug("This is a test message");
+        LOG.debug("Test msg from p9");
     }
 
 }
--- src/main/java/com/apps/P9Collab.java
+++ src/main/java/com/apps/P9Collab.java
@@ -1,4 +1,7 @@
 package com.apps;
 
 public class P9Collab {
+    public static void main(String[] args) {
+        System.out.println("writing a sys out statement");
+    }
 }
