43576ac99f24cebe72b52df094b0954b8792a292
schituku@redhat.com <schituku@redhat.com>
2020-07-02 11:33:14 -0400

    Initial commit

---
+++ src/main/java/com/apps/Log4jTest.java
@@ -0,0 +1,13 @@
+package com.apps;
+
+import org.apache.log4j.Logger;
+
+public class Log4jTest {
+
+    private static final Logger LOG = Logger.getLogger(Log4jTest.class);
+
+    public static void main(String[] args) {
+        LOG.debug("This is a test message");
+    }
+
+}
